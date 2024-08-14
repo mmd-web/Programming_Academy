a31f7b9a060dc6711d87b60299e3a46833befb9c
78 mmd 69 <mmdqurani@gmail.com>
2024-08-15 00:33:16 +0330

    Creating a main css file and also defining the components to component App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
+import Header from './Components/Header/Header';
 
 function App() {
-
   return (
-    <h1>hello</h1>
+    <div className='bg_main w-100 h-100 position-absolute'>
+      <Header></Header>
+    </div>
   )
-  
 }
 
-export default App
+export default App ;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './main.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
