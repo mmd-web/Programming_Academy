8be4b55315cc761529137367e9a15846dcb3b342
78 mmd 69 <mmdqurani@gmail.com>
2024-08-16 18:35:12 +0330

    Changes in the main component

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/js/bootstrap.bundle';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
