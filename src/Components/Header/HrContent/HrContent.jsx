8009e015355b251d662f063203ec7e9b7881e99b
78 mmd 69 <mmdqurani@gmail.com>
2024-08-16 23:52:13 +0330

    Creating shadow animation for header section links and also bolding some texts

--- src/Components/Header/HrContent/HrContent.jsx
+++ src/Components/Header/HrContent/HrContent.jsx
@@ -9,13 +9,13 @@ function HrContent() {
         <div className="row h-100">
           <div className="col-6 justify-content-center align-items-end d-flex flex-column">
             <div className='d-flex h-75 justify-content-end align-items-end flex-column row-gap-3'>
-              <h1 className='text_color_basic'>آکادمی برنامه نویسی دارا</h1>
-              <span className='mt-4 color_white_100 fs-2'>آیدین آسرایی</span>
+              <h1 className='text_color_basic fw-bold'>آکادمی برنامه نویسی دارا</h1>
+              <span className='mt-4 color_white_100 fs-2 fw-bold'>آیدین آسرایی</span>
               <span dir='rtl' className='color_white_50 fs-5 mb-5'>با من ، علم برنامه نویسی رو راحت یاد بگیر و پیشرفت کن.</span>
             </div>
             <div className='w-100 h-25 d-flex justify-content-start align-items-start flex-row-reverse column-gap-3'>
-              <BtnMain className="btn bg_basic color_white_100 w_btn_main py-2" title="ارتباط با من" />
-              <BtnMain className="btn bg_basic_outline w_btn_main py-2" title="دوره های من" />
+              <BtnMain className="btn bg_basic text_color_balck w_btn_main py-2 fw-bold" title="ارتباط با من" />
+              <BtnMain className="btn bg_basic_outline w_btn_main py-2 fw-bold" title="دوره های من" />
             </div>
           </div>
           <div className="col-6 justify-content-center align-items-center d-flex">
--- src/Components/Header/Nav/NavItems.jsx
+++ src/Components/Header/Nav/NavItems.jsx
@@ -4,23 +4,23 @@ export default function NavItems() {
   return (
     <div className='d-lg-flex d-none'>
       <ul className='navbar-nav column-gap-5 flex-row-reverse'>
-        <li className='nav-item'>
-          <a href="#" className='nav-link text-light'>صفحه اصلی</a>
+        <li>
+          <a href="#" className='nav-link text-light shadow_text'>صفحه اصلی</a>
         </li>
         <li>
-          <a href="#" className='nav-link text-light'>خدمات</a>
+          <a href="#" className='nav-link text-light shadow_text'>خدمات</a>
         </li>
         <li>
-          <a href="#" className='nav-link text-light'>فروشگاه</a>
+          <a href="#" className='nav-link text-light shadow_text'>فروشگاه</a>
         </li>
         <li>
-          <a href="#" className='nav-link text-light'>نمونه کارها</a>
+          <a href="#" className='nav-link text-light shadow_text'>نمونه کارها</a>
         </li>
         <li>
-          <a href="#" className='nav-link text-light'>مقالات</a>
+          <a href="#" className='nav-link text-light shadow_text'>مقالات</a>
         </li>
         <li>
-          <a href="#" className='nav-link text-light'>ارتباط با من</a>
+          <a href="#" className='nav-link text-light shadow_text'>ارتباط با من</a>
         </li>
       </ul>
     </div>
