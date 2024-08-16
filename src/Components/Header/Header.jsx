9a97e5bbf87b6709316fb647d2fca2a4dcb1a3d7
78 mmd 69 <mmdqurani@gmail.com>
2024-08-16 18:34:52 +0330

    Changes in the header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from './Navbar';
+import Navbar from './Nav/Navbar';
 
 function Header() {
   return (
-    <header className='w-100 rounded-5'>
+    <header className='w-100 rounded-5 d-flex justify-content-center align-items-center'>
         <Navbar></Navbar>
     </header>
   )
