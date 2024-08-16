a048ecd4ec14d2ca3c6f62ea26c7f306344a3268
78 mmd 69 <mmdqurani@gmail.com>
2024-08-17 00:19:26 +0330

    Responsive header section

--- src/Components/Header/HrContent/HrContent.jsx
+++ src/Components/Header/HrContent/HrContent.jsx
@@ -7,19 +7,26 @@ function HrContent() {
     <div className='w-100 h-100 container'>
       <div className='h_75'>
         <div className="row h-100">
-          <div className="col-6 justify-content-center align-items-end d-flex flex-column">
+          <div className="col-lg-6 col-12 d-flex d-lg-none justify-content-center align-items-center">
+            <div>
+              <img src="../public/images/hrImage/img_517335666206237e4d9.webp" width={100} alt="" />
+            </div>
+          </div>
+          <div className="col-lg-6 col-12 justify-content-center mt_lg px_lg align-items-end d-flex flex-column">
             <div className='d-flex h-75 justify-content-end align-items-end flex-column row-gap-3'>
-              <h1 className='text_color_basic fw-bold'>آکادمی برنامه نویسی دارا</h1>
+              <h1 className='text_color_basic fw-bold' dir='rtl'>آکادمی برنامه نویسی دارا</h1>
               <span className='mt-4 color_white_100 fs-2 fw-bold'>آیدین آسرایی</span>
               <span dir='rtl' className='color_white_50 fs-5 mb-5'>با من ، علم برنامه نویسی رو راحت یاد بگیر و پیشرفت کن.</span>
             </div>
             <div className='w-100 h-25 d-flex justify-content-start align-items-start flex-row-reverse column-gap-3'>
-              <BtnMain className="btn bg_basic text_color_balck w_btn_main py-2 fw-bold" title="ارتباط با من" />
-              <BtnMain className="btn bg_basic_outline w_btn_main py-2 fw-bold" title="دوره های من" />
+              <BtnMain className="btn bg_basic text_color_balck w_btn_main py-2 px_btn_main fw-bold" title="ارتباط با من" />
+              <BtnMain className="btn bg_basic_outline w_btn_main py-2 px_btn_main fw-bold" title="دوره های من" />
             </div>
           </div>
-          <div className="col-6 justify-content-center align-items-center d-flex">
-            <img src="../public/images/hrImage/img_517335666206237e4d9.webp" width={100} alt="" />
+          <div className="col-lg-6 d-lg-flex d-none justify-content-center align-items-center">
+            <div>
+              <img src="../public/images/hrImage/img_517335666206237e4d9.webp" width={100} alt="" />
+            </div>
           </div>
         </div>
       </div>
