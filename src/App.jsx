839285a6e5e23c4275faa37642658119846e4b41
78 mmd 69 <mmdqurani@gmail.com>
2024-08-18 02:08:29 +0330

    Defining the Main component for the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Components/Header/Header';
+import MainBox from './Components/Main/MainBox';
 
 function App() {
   return (
     <div className='bg_main w-100 w_main_100 position-absolute'>
       <Header></Header>
+      <MainBox></MainBox>
     </div>
   )
 }
