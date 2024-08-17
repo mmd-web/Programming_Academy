3ee1b258b4048eebc8125c70885badb2511e344d
78 mmd 69 <mmdqurani@gmail.com>
2024-08-18 02:03:07 +0330

    Create the MainBox component

---
+++ src/Components/Main/MainBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Course from './Course/Course';
+
+function MainBox() {
+  return (
+    <div>
+      <Course></Course>
+    </div>
+  )
+}
+
+export default MainBox;
