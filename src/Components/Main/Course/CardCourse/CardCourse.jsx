0d0ca66d8ca6ffd2a64cc88149bd7c60d43a2f53
78 mmd 69 <mmdqurani@gmail.com>
2024-08-18 21:52:45 +0330

    Refactoring in CardCourse style

--- src/Components/Main/Course/CardCourse/CardCourse.jsx
+++ src/Components/Main/Course/CardCourse/CardCourse.jsx
@@ -4,22 +4,29 @@ import Badge from './Badge/Badge';
 function CardCourse({ ...props }) {
   return (
     <div>
-      <div className="card w_card_course bg_main_two">
+      <div className="card w-100 bg_main_two">
         <img src={props.srcImg} className="card-img-top" alt="..." />
         <div className="card-body py-4 w-100 d-flex row-gap-4 flex-column">
           <div className='w-100 d-flex justify-content-end align-items-center'>
             <h5 className="card-title fs-6 fw-bold color_white_100">{props.title}</h5>
           </div>
           <div className='w-100 d-flex justify-content-start align-items-center'>
-            <span className='color_white_100 fs-6'>{props.stars}</span>
+            <span className='color_white_100 fs-6 d-flex justify-content-center align-items-center column-gap-1'>
+              <img src="../public/images/icon/icons8-star-96.png" className='w_icon_star' alt="" />
+              {props.stars}
+            </span>
           </div>
-          <div className='d-flex justify-content-start align-items-start flex-row'>
-            <div className='h-100 d-flex justify-content-start align-items-end flex-column' style={{width:'40%'}}>
-              <span className='color_white_100 fs-4' dir='rtl'>{props.discountPrice} <span className='fs-6'>تومان</span></span>
-              <span className='fs-5 str_price_main' dir='rtl'>{props.priceMain} </span>
+          <div className='d-flex flex-column'>
+            <div className='h-100 d-flex justify-content-between align-items-center flex-row w-100'>
+              <div>
+                <span className='color_white_100 fs-4' dir='rtl'>{props.discountPrice} <span className='fs-6'>تومان</span></span>
+              </div>
+              <div>
+                <Badge percentage={props.percentageDiscount} ></Badge>
+              </div>
             </div>
-            <div className='h-100 d-flex justify-content-end align-items-center mt-2' style={{width:'60%'}}>
-              <Badge percentage={props.percentageDiscount} ></Badge>
+            <div>
+              <span className='str_price_main fs-6'>{props.priceMain}</span>
             </div>
           </div>
         </div>
