7f9d7f9d8db7d31df50de6c185e0f60ab9fb0958
78 mmd 69 <mmdqurani@gmail.com>
2024-08-19 23:57:55 +0330

    Creating a component for the course introduction section

---
+++ src/Components/Main/Introduction/Introduction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TitlesMain from '../TitlesMain/TitlesMain';
+
+function Introduction() {
+  return (
+    <div>
+      <TitlesMain title="معرفی دوره ها"></TitlesMain>
+    </div>
+  )
+}
+
+export default Introduction;
