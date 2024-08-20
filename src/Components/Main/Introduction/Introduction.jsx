6f8d53e293213c9902bcadf4f02ee360d88b3a2f
78 mmd 69 <mmdqurani@gmail.com>
2024-08-20 22:43:53 +0330

    Defining and using the IntBox component and defining the values of each item

--- src/Components/Main/Introduction/Introduction.jsx
+++ src/Components/Main/Introduction/Introduction.jsx
@@ -4,21 +4,21 @@ import IntBox from './IntBox/IntBox';
 
 function Introduction() {
   return (
-    <div className='bg-danger w-100 h-100 py-5 px-1'>
+    <div className='w-100 h-100 py-5 px-1'>
       <TitlesMain title="معرفی دوره ها"></TitlesMain>
       <div className='w-100 h-100 mt-5'>
         <div className="row row-gap-4">
-          <div className="col-6">
-            <IntBox></IntBox>
+          <div className="col-md-6 col-12">
+            <IntBox srclink="public/images/icon/IconIntroduction/icons8-javascript-96.png" title="JavaScript" colorOne="#FDA636" colorTwo="#F3315A"></IntBox>
           </div>
-          <div className="col-6">
-            <IntBox></IntBox>
+          <div className="col-md-6 col-12">
+            <IntBox srclink="public/images/icon/IconIntroduction/icons8-node-js-96.png" title="NoodJs" colorOne="#2FC6E3" colorTwo="#28E266"></IntBox>
           </div>
-          <div className="col-6">
-            <IntBox></IntBox>
+          <div className="col-md-6 col-12">
+            <IntBox srclink="public/images/icon/IconIntroduction/icons8-vuejs-100.png" title="VueJs" colorOne="#1AB372" colorTwo="#316987"></IntBox>
           </div>
-          <div className="col-6">
-            <IntBox></IntBox>
+          <div className="col-md-6 col-12">
+            <IntBox srclink="public/images/icon/IconIntroduction/icons8-atom-68.png" title="ReactJs" colorOne="#3399FD" colorTwo="#9839F8"></IntBox>
           </div>
         </div>
       </div>
