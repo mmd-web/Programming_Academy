a9883d5570fcfb11482f9981c256f0e07ab79d98
78 mmd 69 <mmdqurani@gmail.com>
2024-08-20 22:41:23 +0330

    Completing the IntBox component to introduce courses

--- src/Components/Main/Introduction/IntBox/IntBox.jsx
+++ src/Components/Main/Introduction/IntBox/IntBox.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './IntBox.css'
 
-function IntBox() {
+function IntBox({...props}) {
   return (
-    <div className='bg-warning'>IntBox</div>
+    <a href='#' className='w_intBox text-decoration-none rounded-4 overflow-hidden d-flex justify-content-center align-items-center flex-row' style={{backgroundImage:`linear-gradient(to right, ${props.colorOne} , ${props.colorTwo})`}}>
+      <div className='h-100 d-flex justify-content-center align-items-center' style={{width:"40%"}}>
+        <div className='w-75 h-75'>
+          <img src={props.srclink} className='w-100 h-100 object-fit-cover' alt="" />
+        </div>
+      </div>
+      <div className='h-100' style={{width:"60%"}}>
+        <div className='w-100 d-flex justify-content-center pe-1 align-items-xl-end align-items-center' style={{height:"60%"}}>
+          <span className='fw-bold color_white_100 fs_1_991 w-100 me-3' dir='rtl'>آموزش {props.title} </span>
+        </div>
+        <div className='w-100 d-flex justify-content-end pe-1 align-items-center' style={{height:"40%"}}>
+          <a href="#" className='btn rounded-4 btn-light px-4 me-3'>
+            <span className='text_color_balck fw-bold'>مشاهده</span>
+          </a>
+        </div>
+      </div>
+    </a>
   )
 }
 
