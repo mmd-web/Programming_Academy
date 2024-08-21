c0a2c3cd269e88a99f812e8ce010a11d291e1cd6
78 mmd 69 <mmdqurani@gmail.com>
2024-08-21 23:29:59 +0330

    Defining and using the CourseFeatures component

--- src/Components/Main/MainBox.jsx
+++ src/Components/Main/MainBox.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Course from './Course/Course'
 import Introduction from './Introduction/Introduction';
+import CourseFeatures from './CourseFeatures/CourseFeatures';
 
 function MainBox() {
   return (
     <div className='container'>
       <Course></Course>
       <Introduction></Introduction>
+      <CourseFeatures></CourseFeatures>
     </div>
   )
 }
