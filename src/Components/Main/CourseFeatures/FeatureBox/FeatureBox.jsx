602b8021f6b3bf8649a1b16c611defe9df605c91
78 mmd 69 <mmdqurani@gmail.com>
2024-08-22 23:34:48 +0330

    Completing the design of the /Features of Courses/ section

--- src/Components/Main/CourseFeatures/FeatureBox/FeatureBox.jsx
+++ src/Components/Main/CourseFeatures/FeatureBox/FeatureBox.jsx
@@ -9,7 +9,14 @@ export default function FeatureBox({ ...props }) {
               <img src={props.srclink} style={{width:'80%'}} className='object-fit-cover' alt="" />
           </div>
         </div>
-        <div className="col-9 h-100 bg-primary"></div>
+        <div className="col-9 h-100 d-flex justify-content-center align-items-start flex-column row-gap-3">
+          <div>
+            <span className='fs-4 fw-bold text_color_basic'>{props.title}</span>
+          </div>
+          <div style={{width:'90%'}}>
+            <span className='fs-6 fw-bold color_white_50 lh-lg'>{props.subTitle}</span>
+          </div>
+        </div>
       </div>
     </div>
   )
