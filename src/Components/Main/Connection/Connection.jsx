116a5e8441d108256f3de1a419847a751d941130
78 mmd 69 <mmdqurani@gmail.com>
2024-08-25 01:14:17 +0330

    Create the Connection component

---
+++ src/Components/Main/Connection/Connection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TitlesMain from '../TitlesMain/TitlesMain'
+
+export default function Connection() {
+  return (
+    <div className='w-100 h-100 py-5 mt-3'>
+      <TitlesMain title="ارتباط با ما"></TitlesMain>
+      <div className='w-100 h-100 mt-5'>
+        
+      </div>
+    </div>
+  )
+}
