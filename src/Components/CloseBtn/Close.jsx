6d3e811cb3c8cb5efd95f2fed98fd8c9d3c736a5
78 mmd 69 <mmdqurani@gmail.com>
2024-08-27 22:20:54 +0330

    Refactoring Close component codes

--- src/Components/CloseBtn/Close.jsx
+++ src/Components/CloseBtn/Close.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 function Close({target , id}) {
   return (
     <div>
-      <button className="navbar-toggler bg-danger ms-4" type="button" data-bs-toggle={target} data-bs-target={'#'+id}>
-        <span className="navbar-toggler-icon"></span>
+      <button className="navbar-toggler border-0 ms-4" type="button" data-bs-toggle={target} data-bs-target={'#'+id}>
+        {/* <span className="navbar-toggler-icon"></span> */}
+        <img src="src/assets/images/icon/icons8-menu-50 (1).png" style={{width:'30px'}} alt="" />
       </button>
     </div>
   )
