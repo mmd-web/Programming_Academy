7be242dd5f620f1f0f1a24e1fcf9c8218c6b0164
78 mmd 69 <mmdqurani@gmail.com>
2024-08-27 22:21:33 +0330

    Refactoring Offcanvas component codes

--- src/Components/Header/Nav/Offcanvas.jsx
+++ src/Components/Header/Nav/Offcanvas.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
+import Logo from './Logo';
 
 function Offcanvas() {
   return (
     <div className='d-lg-none d-flex'>
       <div className="offcanvas offcanvas-start bg-dark text-light" tabindex="-1" id="offcanvasExample">
-        <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+        <div className="offcanvas-header px-3">
+          <Logo></Logo>
           <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
         </div>
         <div className="offcanvas-body">
           <div>
-            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            <ul className='navbar-nav flex-column row-gap-3'>
+              <li>
+                <a href="#" className='nav-link text-light shadow_text bg_main_two px-3 py-3 rounded-3'>صفحه اصلی</a>
+              </li>
+              <li>
+                <a href="#" className='nav-link text-light shadow_text bg_main_two px-3 py-3 rounded-3'>خدمات</a>
+              </li>
+              <li>
+                <a href="#" className='nav-link text-light shadow_text bg_main_two px-3 py-3 rounded-3'>فروشگاه</a>
+              </li>
+              <li>
+                <a href="#" className='nav-link text-light shadow_text bg_main_two px-3 py-3 rounded-3'>نمونه کارها</a>
+              </li>
+              <li>
+                <a href="#" className='nav-link text-light shadow_text bg_main_two px-3 py-3 rounded-3'>مقالات</a>
+              </li>
+              <li>
+                <a href="#" className='nav-link text-light shadow_text bg_main_two px-3 py-3 rounded-3'>ارتباط با من</a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
