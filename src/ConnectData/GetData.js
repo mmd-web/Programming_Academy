fbef157c62a97ed2c905fb48287433d00cf23554
78 mmd 69 <mmdqurani@gmail.com>
2024-11-06 19:38:41 +0330

    Creating a component to call Storage data

---
+++ src/ConnectData/GetData.js
@@ -0,0 +1,25 @@
+import { supabase } from "../API/supabase";
+
+export default async function GetStorage( state ){
+  const { data } = await supabase
+    .storage
+    .from('files')
+    .list('')
+    state.setState({
+      storageFiles: data
+    })
+  console.log(data);
+  
+  return data;
+}
+
+export let saveRequired ;
+
+export function RequiredData ( data , type ){
+  saveRequired = data.map(item => item).filter(img => {
+    return img.name.startsWith(type)
+  })
+  console.log(saveRequired);
+  
+  return saveRequired;
+}
