01cbf512e86d09ad94b6d361a28933322c4908cd
78 mmd 69 <mmdqurani@gmail.com>
2024-11-14 13:35:44 +0330

    Refactoring the structure of the Introduction component, and connecting the Introduction component to the Api

--- src/Components/Main/Introduction/Introduction.jsx
+++ src/Components/Main/Introduction/Introduction.jsx
@@ -1,34 +1,67 @@
 import React from 'react'
 import TitlesMain from '../TitlesMain/TitlesMain';
 import IntBox from './IntBox/IntBox';
-import GetImg from '../../GetImg/GetImg.jsx';
+import App from '../../../App';
+import { RequiredData, saveRequired } from '../../../ConnectData/GetData';
 
-// let valuesInBox = [
-//   { id: 1, srcImg: IconIntroduction1, title: 'JavaScript', colorOne: '#FDA636', colorTwo: '#F3315A' },
-//   { id: 2, srcImg: IconIntroduction2, title: 'NoodJs', colorOne: '#2FC6E3', colorTwo: '#28E266' },
-//   { id: 3, srcImg: IconIntroduction3, title: 'VueJs', colorOne: '#1AB372', colorTwo: '#316987' },
-//   { id: 4, srcImg: IconIntroduction4, title: 'ReactJs', colorOne: '#3399FD', colorTwo: '#9839F8' },
-// ]
+class Introduction extends React.Component {
+  constructor(props) {
+    super(props);
 
-function Introduction() {
-  return (
-    <div className='w-100 h-100 py-5 px-1'>
-      <TitlesMain title="معرفی دوره ها"></TitlesMain>
-      <div className='w-100 h-100 mt-5'>
-        <div className="row row-gap-4">
-          {/* {
-            valuesInBox.map(item => {
-              return (
+    this.state = {
+      products: [],
+      storageFile: [],
+      colorBoxIntroduction : [
+        {colorOne : 'rgb(253, 166, 54)' , colorTwo : 'rgb(243, 49, 90)'},
+        {colorOne : 'rgb(47, 198, 227)' , colorTwo : 'rgb(40, 226, 102)'},
+        {colorOne : 'rgb(26, 179, 114)' , colorTwo : 'rgb(49, 105, 135)'},
+        {colorOne : 'rgb(51, 153, 253)' , colorTwo : 'rgb(152, 57, 248)'}
+      ]
+    }
+
+    this.callAppComponent = new App();
+
+    this.callProductsTable.bind(this);
+    this.callProductsTable()
+    this.callStorageItem.bind(this);
+    this.callStorageItem()
+  }
+
+  async callProductsTable() {
+    this.setState({
+      products: await this.callAppComponent.getProductList()
+    })
+  }
+
+  async callStorageItem() {
+    this.setState({
+      storageFile: await this.callAppComponent.getStorage()
+    })
+  }
+
+  render() {
+    RequiredData(this.state.storageFile, 'Introduction');
+    return (
+      <div className='w-100 h-100 py-5 px-1'>
+        <TitlesMain title="معرفی دوره ها"></TitlesMain>
+        <div className='w-100 h-100 mt-5'>
+          <div className="row row-gap-4">
+            {this.state.products &&
+              this.state.products.map(item =>
                 <div key={item.id} className="col-md-6 col-12">
-                  <IntBox srclink={GetImg(item.srcImg)} title={item.title} colorOne={item.colorOne} colorTwo={item.colorTwo}></IntBox>
+                  {console.log(saveRequired[item.id - 1])}
+                  {item.id < saveRequired.length+1 ?
+                    <IntBox src={saveRequired[item.id - 1]} {...item} {...this.state.colorBoxIntroduction[item.id - 1]}></IntBox>
+                    : null
+                  }
                 </div>
               )
-            })
-          } */}
+            }
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Introduction;
