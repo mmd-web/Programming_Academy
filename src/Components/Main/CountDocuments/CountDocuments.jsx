4d2908ed5d150d2322adde624e1d4b3994339d3a
78 mmd 69 <mmdqurani@gmail.com>
2024-11-14 19:55:03 +0330

    Refactoring the CountDocuments component as well as calling the required information from the API

--- src/Components/Main/CountDocuments/CountDocuments.jsx
+++ src/Components/Main/CountDocuments/CountDocuments.jsx
@@ -1,25 +1,50 @@
 import React from 'react'
 import TitlesMain from "../TitlesMain/TitlesMain"
 import BoxImgDocs from './BoxImgDocs/BoxImgDocs'
+import App from '../../../App'
+import { RequiredData , saveRequired } from '../../../ConnectData/GetData'
 import './CountDocuments.css'
+import { StorageAddres } from '../Course/CardCourse/CardCourse'
 
-export default function CountDocuments() {
-  return (
-    <div className='w-100 h-100 py-5 mt-3'>
-      <TitlesMain title="تعدادی از مدارک اعطا شده"></TitlesMain>
-      <div className='w-100 h-100 mt-5'>
-        <div className="row row-gap-4">
-          {/* {
-            valueBoxImgDocs.map(item => {
-              return (
-                <div key={item.id} className="col-xl-3 col-md-6 col-12">
-                  <BoxImgDocs srclink={item.srcImg}></BoxImgDocs>
-                </div>
-              )
-            })
-          } */}
+export default class CountDocuments extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      storageFile : []
+    }
+
+    this.callAppComponent = new App();
+
+    this.callStorageItem.bind(this);
+    this.callStorageItem()
+  }
+
+  async callStorageItem(){
+    this.setState({
+      storageFile : await this.callAppComponent.getStorage()
+    })
+  }
+
+  render(){
+    RequiredData(this.state.storageFile , 'doc')
+    return (
+      <div className='w-100 h-100 py-5 mt-3'>
+        <TitlesMain title="تعدادی از مدارک اعطا شده"></TitlesMain>
+        <div className='w-100 h-100 mt-5'>
+          <div className="row row-gap-4">
+            {saveRequired && 
+              saveRequired.map(item => {
+                return (
+                  <div key={item.id} className="col-xl-3 col-md-6 col-12">
+                    <BoxImgDocs srclink={`${StorageAddres}${item.name}`}></BoxImgDocs>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
